a55e06e70a806dfc05e8c3d880fcba3705d747cd
r0m1mPL <simachenko.roma@gmail.com>
2022-06-16 19:26:13 +0300

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import bot
 
 def main():
     products = scraping.collect_products()
-    print(f"[INFO] Всего {len(products)} товаров удалось собрать...")
+    print(f"[INFO] Всего {len(products)} товаров удалось собрать.")
     bot.post_to_telegram(products)
 
 
